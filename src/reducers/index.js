cd83a49574a29f2de96eaa240379b6a5e31702a0
Chetan Vekariya <chetan.vekariya@dsquaresolutions.com>
2018-04-22 23:31:07 +0530

    Removed use of routerReducer as reducer in the app which is of no use here

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-import { routerReducer } from 'react-router-redux';
 
 import QuizReducer from "./reducer_quiz";
 
 const rootReducer = combineReducers({
-    quizData: QuizReducer,
-    routing: routerReducer,
+    quizData: QuizReducer
 });
 
 export default rootReducer;
